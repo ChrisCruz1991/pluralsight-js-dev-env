bfcbd9061f177a60120d1e360fcf24a3b3f85af6
Christopher Cruz Rubert <christopher.cruz2@upr.edu>
2018-08-28 15:29:00 -0400

    Setup .babelrc

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${PORT}`);
+  }
+});
